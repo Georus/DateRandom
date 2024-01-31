9dd36259e1bf9dba140264cd4ab073fba2023f96
Jorge Ruiz <jorgeruizsalcedo@gmail.com>
2024-01-30 19:26:20 -0700

    Styled right

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -128,6 +128,6 @@ def update(id):
 def delete(id):
     get_post(id)
     db = get_db()
-    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.execute('DELETE FROM activities WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('blog.index'))
